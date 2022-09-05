de845a9d7c9737027e8b1be1d9c29cc64a71f9a8
Carol <carolinemaee@live.com>
2022-09-05 11:39:38 +0930

    finalised route

--- routes/api/tagRoutes.js
+++ routes/api/tagRoutes.js
@@ -1,9 +1,84 @@
 const router = require('express').Router();
-const { Category, Product, Tag } = require('../../models');
+const { Product, Tag, ProductTag } = require('../../models');
 
+router.get('/', async (req, res) => {
+    try {
+        const tagData = await Tag.findAll({
+            include: [{ model: Product, through: ProductTag, as: 'products' }]
+        });
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
+router.get('/:id', async (req, res) => {
+    try {
+        const tagData = await Tag.findByPk(req.params.id, {
+            include: [{ model: Product, through: ProductTag, as: 'products' }]
+        });
 
+        if (!tagData) {
+            res.json(404).json({ message: 'No tag found with this id!' });
+            return;
+        }
 
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
+router.post('/', async (req, res) => {
+    try {
+        const tagData = await Tag.create(req.body);
+        res.status(200).json(tagData)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const tagData = await Tag.update(
+            {
+                tag_name: req.body.tag_name
+            },
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        )
+
+        if (!tagData) {
+            res.status(404).json({ message: 'No tag found with that id!' });
+            return;
+        }
+
+        res.status(200).json(tagData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const tagData = await Tag.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+
+        if (!tagData) {
+            res.status(404).json({ message: 'No tag found with that id!' });
+            return;
+        }
+
+        res.status(200).json(tagData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router;
